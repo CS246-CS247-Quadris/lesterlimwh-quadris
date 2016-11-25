1110b7094fb69ac10c02fec65379693ca0640db9
PranavTripathi101 <tripathi_pranav@yahoo.ca>
2016-11-25 13:47:52 -0500

    changed level.cc and level.h

--- level.h
+++ level.h
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include "blockcoord.h"
+#include <vector>
 class Block;
 
 class Level{
+ static int count = 0;
  int dif;
  Grid *g;
- BlockCoord *b;
  ifstream f;
+ std::vector<string> seqInput;
  Block* diff0Block();
  Block* diff1Block();
  Block* diff2Block();
@@ -15,7 +17,9 @@ class Level{
  Block* diff4Block();
  public:
   Level(int n);
+  Level(int n, ifstream &f);
   ~Level();
+  void readInFile();
   void levelUp();
   void levelDown();
   Block* makeBlock();
