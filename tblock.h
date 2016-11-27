84e0051490898045ddca6aca3deeade369a26545
SamatarUW <samatar.abukar@gmail.com>
2016-11-27 18:29:36 -0500

    Added Tblock and updated Sblock and Iblock rotations but still incomplete

---
+++ tblock.h
@@ -0,0 +1,26 @@
+#ifndef __TBLOCK_H__
+#define __TBLOCK_H__
+#include "grid.h"
+#include "block.h"
+#include "blockcoord.h"
+
+class Tblock {
+	BlockCoord coords;
+	bool isHeavy;
+	Grid *g;
+	int orientation = 0;
+	const char name = 'T';
+
+public:
+	Tblock(BlockCoord coords, bool isHeavy, Grid *g);
+	void left() override;
+	void right() override;
+	void down() override;
+	void counterclockwise() override;
+	void clockwise() override;
+	void drop() override;
+	BlockCoord getBlockCoord() override;
+	char getBlockType() override;
+};
+
+#endif
