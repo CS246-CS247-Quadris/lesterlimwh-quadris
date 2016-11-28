706f6962e6a60eb0cf019553b3822fd62932ad37
Lester Lim <lesterlimwh@gmail.com>
2016-11-27 20:18:50 -0500

    Rowclear and Sblock needs to change

--- tblock.cc
+++ tblock.cc
@@ -120,19 +120,11 @@ void Tblock::clockwise() {
 		temp = {x1,x2,x3,x4};
 		tempOrientation = 3;
 	} 
-<<<<<<< HEAD
 	if (orientation == 3) {
 		Coord x1{coords.x1.x+1,coords.x1.y-1};
 		Coord x2{coords.x2.x,coords.x2.y};
 		Coord x3{coords.x3.x-1,coords.x3.y+1};
 		Coord x4{coords.x4.x+1,coords.x4.y+1};
-=======
-	if (orientation == 1) {
-		Coord x1{coords.x1.x+1,coords.x1.y-1};
-		Coord x2{coords.x2.x,coords.x2.y};
-		Coord x3{coords.x3.x-1,coords.x3.y+1};
-		Coord x4{coords.x4.x-+1,coords.x4.y+1};
->>>>>>> 225c853b30c88e00593005644674272f13710056
 
 		temp = {x1,x2,x3,x4};
 		tempOrientation  = 2;
@@ -146,11 +138,7 @@ void Tblock::clockwise() {
 		temp = {x1,x2,x3,x4};
 		tempOrientation = 1;
 	}
-<<<<<<< HEAD
 	if (orientation == 1) {
-=======
-	if (orientation == 3) {
->>>>>>> 225c853b30c88e00593005644674272f13710056
 		Coord x1{coords.x1.x-1,coords.x1.y+1};
 		Coord x2{coords.x2.x,coords.x2.y};
 		Coord x3{coords.x3.x+1,coords.x3.y-1};
